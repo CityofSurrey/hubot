417526a7c2ea3ac9992eac0e109516d9eaef5813
Viatcheslav Ivanov <slava.eth@gmail.com>
2016-04-25 01:40:58 -0700

    feat(): add deploy commands (#8)

--- app/scripts/_bootstrap.js
+++ app/scripts/_bootstrap.js
@@ -1,6 +1,7 @@
 // Description:
-//   Initializes hubot scripts with es6 syntax
+//   Initializes hubot scripts with es6 support
 
 require('babel-register')
+require('babel-polyfill')
 
 module.exports = () => {}
